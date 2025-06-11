fe945b26176ff3fd2d1ea456df7a04be899f8b90
Arman Yazdi <rmnyzd@gmail.com>
2025-06-11 18:25:16 +0330

    style: format code with gofumpt

--- hello-world/main.go
+++ hello-world/main.go
@@ -5,15 +5,15 @@ import (
 )
 
 func main() {
-    // Initialize a new Gonoleks app
+	// Initialize a new Gonoleks app
 	app := gonoleks.New()
 
-    // Define a route for the GET method on the root path '/'
+	// Define a route for the GET method on the root path '/'
 	app.GET("/", func(c *gonoleks.Context) {
-        // Send a string response with status code 200
-        c.String(200, "Hello, World!")
+		// Send a string response with status code 200
+		c.String(200, "Hello, World!")
 	})
 
-    // Start the server on port 3000
+	// Start the server on port 3000
 	app.Run(":3000")
 }
