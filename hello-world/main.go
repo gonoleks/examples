64e238b57673dccc004dacf42fb0c1f9318d5106
Arman Yazdi <rmnyzd@gmail.com>
2025-06-11 13:29:23 +0330

    Add hello-world

---
+++ hello-world/main.go
@@ -0,0 +1,19 @@
+package main
+
+import (
+	"github.com/gonoleks/gonoleks"
+)
+
+func main() {
+    // Initialize a new Gonoleks app
+	app := gonoleks.New()
+
+    // Define a route for the GET method on the root path '/'
+	app.GET("/", func(c *gonoleks.Context) {
+        // Send a string response with status code 200
+        c.String(200, "Hello, World!")
+	})
+
+    // Start the server on port 3000
+	app.Run(":3000")
+}
